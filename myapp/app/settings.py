3bb708726e5f56acc999f2ef9694943f1a97eacf
bongsu <zio7542@naver.com>
2023-12-12 16:09:12 +0900

    test : 배포 test

--- myapp/app/settings.py
+++ myapp/app/settings.py
@@ -36,7 +36,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*'
+    ".ap-northeast-2.compute.amazonaws.com"
+]
 # 
 # Application definition
 
